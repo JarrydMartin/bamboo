1700d9b0f77b3e38ef93d0fb3510eec25a7b89d8
Jarryd Martin <jarryd@codora.co.nz>
2021-07-13 19:24:26 +1000

    Added GitHub cli to create a new repo

--- dispatch-action/newAction.ts
+++ dispatch-action/newAction.ts
@@ -4,18 +4,29 @@ export const newAction = async (options: string[]) => {
 
   if (projectName) {
     console.log(`🌟 Starting new ${projectName} project...\n`);
-    await Deno.mkdir(projectName);
+    
+    const pGitHub = Deno.run({
+      cmd: ["gh", "repo", "create", projectName, '-y']
+    })
+
+     await pGitHub.status()
+     pGitHub.close()
+
     Deno.chdir(`./${projectName}`);
 
     const pNpx = Deno.run({
       cmd: ["npx", "create-react-app", "app", "--template", "typescript"],
     });
     await pNpx.status();
+    pNpx.close();
+
+    
 
     const pCode = Deno.run({
       cmd: ["code", "."],
     });
     await pCode.status();
+    pCode.close();
   } else {
     console.log(
 "Oops! 😮 \n\
